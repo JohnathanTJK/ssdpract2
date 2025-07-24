c7959636e907a971a4a577917e2c9c2c2be21f07
JohnathanTJK <64633864+JohnathanTJK@users.noreply.github.com>
2025-07-25 02:08:05 +0800

    updated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,34 @@
 import js from "@eslint/js";
 import globals from "globals";
+import security from "eslint-plugin-security";
+import securityNode from "eslint-plugin-security-node";
+import noUnsanitized from "eslint-plugin-no-unsanitized";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node, ...globals.mocha} } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.mocha,
+      },
+    },
+    plugins: {
+      js,
+      security,
+      "security-node": securityNode,
+      "no-unsanitized": noUnsanitized,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...security.configs.recommended.rules,
+      ...securityNode.configs.recommended.rules,
+      ...noUnsanitized.configs.recommended.rules, 
+      // Manually enable rules from no-unsanitized plugin
+      "no-unsanitized/method": "error",
+      "no-unsanitized/property": "error",
+    },
+  },
 ]);
